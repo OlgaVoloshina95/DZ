b89f50a3a0de71b352934dc78372da0767327162
Olga Voloshina <lapolka@bk.ru>
2022-08-17 12:45:53 +0300

    Task29 done

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Ниже массив из случайных чисел");
+Random rnd=new Random();
+int[] array=new int[1];
+for (int i=0;i<array.Length;i++)
+{
+    array[i]=rnd.Next();
+    Console.WriteLine(array[i]); 
+}
