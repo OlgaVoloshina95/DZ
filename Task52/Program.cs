b3cf5190a06b03d65748096f3f565b35e551a921
Olga Voloshina <lapolka@bk.ru>
2022-09-02 14:00:41 +0300

    Task52 done

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -2,43 +2,61 @@
 
 
 
-int[,] arrayWhole = new int[m, n];
-arrayWhole = TransformationArrayWhole(array);
+Console.WriteLine($"Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
 
-WriteArrayInt(arrayWhole);
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"\nCреднее арифметическое:\n");
-for (int i = 0; i < n; i++)
-{
-  double arithmeticMean = 0;
-  for (int j = 0; j < m; j++)
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+double[,] array = new double[m, n];
+CreateArrayDouble(array);
+WriteArray(array);
+Console.WriteLine();
+Console.Write("Среднее арифметическое столбцов: ");
+int x=0;
+while (x<array.GetLength(0))
   {
-    arithmeticMean += arrayWhole[j, i];
+    Console.Write($" {Math.Round(ArithmeticMean(x,array),1)}");
+    x++;
   }
-  arithmeticMean = Math.Round(arithmeticMean / m, 1);
-  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
-}
 
-int[,] TransformationArrayWhole (double[,] array)
+//Функции
+void CreateArrayDouble(double[,] array)
 {
-  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < m; i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < n; j++)
     {
-      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+      array[i, j] = new Random().NextDouble() * 20 - 10;
     }
   }
-  return arrayWhole;
 }
 
-void WriteArrayInt (int[,] arrayWhole){
+void WriteArray (double[,] array)
+{
 for (int i = 0; i < m; i++)
   {
       for (int j = 0; j < n; j++)
       {
-        Console.Write(arrayWhole[i, j] + " ");
+        double alignNumber = Math.Round(array[i, j], 1);
+        Console.Write(alignNumber + " ");
       }
       Console.WriteLine();
   }
+}
+
+double ArithmeticMean(int i,double[,] array)
+{double am=0;
+int j=0;
+while (j<array.GetLength(1))
+  {
+    am=am+array[i,j];
+    j++;
+  }
+  am=am/array.GetLength(1);
+  return am;
+
 }
